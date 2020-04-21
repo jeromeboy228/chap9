4409baebc0f83fdd420013244a48f4d8d41788e9
jeromeboy228 <egorbaranov77@gmail.com>
2020-04-21 18:40:17 +0200

    new add

--- chap9_prac1.c
+++ chap9_prac1.c
@@ -1,29 +1,5 @@
 #include <stdio.h>
-<<<<<<< HEAD
-void min(float, float);
-int main(int argc, char const *argv[])
-{
-    float n1, n2;
-    float minim;
-    printf("Enter 2 numbers\t");
-    while (scanf("%f%f",&n1,&n2) == 2)
-    {
-        min(n1, n2);
-        printf("Enter 2 numbers(or q for escape\t");
-    }
-    putchar('\n');
-    return 0;
-}
-void min(float a, float b)
-{
-    if (a>b)
-    {
-        printf("%.03f меньше\n",b);
-    }
-    else
-    printf("%.03f меньше\n",a);
-    
-=======
+
 int min(int, int);
 int main()
 {
@@ -43,5 +19,4 @@ int min(int n1, int n2){
         return n2;
     }
     return n1;
->>>>>>> f9cbcf8a45d5d2d4ed4b561ba6302fc0d85771f6
 }
