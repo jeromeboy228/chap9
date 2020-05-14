633e68fe1bca8c8a84b3e745b15584daad3fa63e
jeromeboy228 <egorbaranov77@gmail.com>
2020-05-14 16:18:06 +0300

    n

--- chap9_prac7.c
+++ chap9_prac7.c
@@ -5,7 +5,13 @@ int main(int argc, char const *argv[])
     char r;
     while ((r=getchar())!=EOF)
     {
-        putchar(r);
+        if (r==' ')
+        {
+            printf("_");
+            continue;
+        }
+        
+        printf("%c = %d  ",r,r);
     }
     
     return 0;
