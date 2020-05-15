5a018ad4416ea7e059457e2d30178899c6efefdf
jeromeboy228 <egorbaranov77@gmail.com>
2020-05-15 15:10:21 +0300

    c7

--- chap9_prac7.c
+++ chap9_prac7.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-
+#include <ctype.h>
+#define alf_start 98
+ void scan_file();
 int main(int argc, char const *argv[])
 {
-    char r;
-    while ((r=getchar())!=EOF)
-    {
-        if (r==' ')
-        {
-            printf("_");
-            continue;
-        }
-        
-        printf("%c = %d  ",r,r);
-    }
+    char a='a';
+    printf("a = %d\n",a);
+    printf("буква %c по %d по счету\n",a,alf_start-a);
+    scan_file();
     
     return 0;
+}
+void scan_file(){
+    char c;
+    while ((c=getchar())!=EOF)
+    {
+        printf("%c",c);
+    }
+    
 }
